d9c48d6d752063466b3b196afb99574667b038c7
Tzu-ping Chung <uranusjr@gmail.com>
2016-04-11 18:01:48 +0800

    Better GunicornRunner encapsulation

--- djgunicorn/gunicorn.py
+++ djgunicorn/gunicorn.py
@@ -3,32 +3,36 @@ import subprocess
 from django.conf import settings
 
 
-def build_gunicorn_arguments(addr, port, options):
-    handle_static_files = (
-        options['use_static_handler'] and
-        (settings.DEBUG or options['insecure_serving'])
-    )
-    app_name = 'static_handler' if handle_static_files else 'django_handler'
-    args = [
-        'djgunicorn.wsgi:' + app_name,
-        '--bind', '{addr}:{port}'.format(addr=addr, port=int(port)),
-        '--access-logfile', '-',
-        '--access-logformat', '%(t)s "%(r)s" %(s)s %(B)s',
-        '--error-logfile', '-',
-        '--log-level', 'warning',
-    ]
-    if options['use_reloader']:
-        args.append('--reload')
-    return args
-
-
 class GunicornRunner(object):
 
-    gunicorn = 'gunicorn'
+    executable = 'gunicorn'
 
-    def __init__(self, args):
-        args = [self.gunicorn] + args
+    def __init__(self, addr, port, ipv6, options):
+        args = self.build_arguments(addr, port, ipv6, options)
         self.proc = subprocess.Popen(args, universal_newlines=True)
+
+    def build_arguments(self, addr, port, ipv6, options):
+        handle_statics = (
+            options['use_static_handler'] and
+            (settings.DEBUG or options['insecure_serving'])
+        )
+        app_name = 'static_handler' if handle_statics else 'django_handler'
+        addrport = (
+            '[{addr}]:{port}' if ipv6
+            else '{addr}:{port}'
+        ).format(addr=addr, port=int(port))
+        args = [
+            self.executable,
+            'djgunicorn.wsgi:' + app_name,
+            '--bind', addrport,
+            '--access-logfile', '-',
+            '--access-logformat', '%(t)s "%(r)s" %(s)s %(B)s',
+            '--error-logfile', '-',
+            '--log-level', 'warning',
+        ]
+        if options['use_reloader']:
+            args.append('--reload')
+        return args
 
     def run(self):
         self.proc.wait()
@@ -37,6 +41,5 @@ class GunicornRunner(object):
 def run(addr, port, options, ipv6=False, threading=False):
     """Patched runserver internal with Gunicorn.
     """
-    args = build_gunicorn_arguments(addr=addr, port=port, options=options)
-    runner = GunicornRunner(args)
+    runner = GunicornRunner(addr=addr, port=port, ipv6=ipv6, options=options)
     runner.run()
