887c38bfcee7adf2ca1dde466ff7563a9fb5fcec
Tzu-ping Chung <uranusjr@gmail.com>
2016-04-11 14:05:56 +0800

    Make use of ParseResult's named arrtibutes

--- djgunicorn/wsgi.py
+++ djgunicorn/wsgi.py
@@ -40,7 +40,7 @@ class StaticFilesWSGIHandler(WSGIHandler):
         * the host is provided as part of the base_url
         * the request's path isn't under the media path (or equal)
         """
-        return path.startswith(self.base_url[2]) and not self.base_url[1]
+        return path.startswith(self.base_url.path) and not self.base_url.netloc
 
 
 django_handler = get_internal_wsgi_application()
